121e21d83c1278a888542c82f768d16d96178eb1
onlinetrainingsj@gmail.com <onlinetrainingsj@gmail.com>
2026-07-29 22:58:14 +0530

    added space

--- TestApril2026Batch/src/test/java/testPackage/TestWebDriverManager.java
+++ TestApril2026Batch/src/test/java/testPackage/TestWebDriverManager.java
@@ -11,6 +11,7 @@ public class TestWebDriverManager {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://testautomationpractice.blogspot.com/");
+		
 
 	}
 
